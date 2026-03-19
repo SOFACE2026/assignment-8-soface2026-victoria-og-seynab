0fdbf2bae39e082dd5fd887fc54d93f34c7bbaec
Seyn0012 <au800162@uni.au.dk>
2026-03-19 12:28:02 +0100

    Tilføjet afsendernavn til chat client, Seynab

--- src/chat_client.cpp
+++ src/chat_client.cpp
@@ -13,7 +13,8 @@ zmq::context_t ctx;
 std::map<std::string,zmq::socket_t> name_to_socket;
 std::mutex name_to_socket_lock;
 
-void send_func(std::string whereis_endpoint)
+// The name 
+void send_func(std::string whereis_endpoint, std::string my_name)
 {   
 
     zmq::socket_t sock(ctx,zmq::socket_type::req);
@@ -64,7 +65,10 @@ void send_func(std::string whereis_endpoint)
         {
             maybe_socket = name_to_socket.find(recipient);
             std::cout << "sending message: '" << text << "' to: '" << recipient << "'" << std::endl; 
-            maybe_socket->second.send(zmq::buffer(text));
+
+            // Changed here to see the senders name
+            std::string message_with_name = my_name + "," + text;
+            maybe_socket->second.send(zmq::buffer(message_with_name));
         }
     }
     
@@ -80,7 +84,20 @@ void recv_func(std::string endpoint)
     while(true)
     {
         auto _ = sock.recv(msg);
+
+         // Find the comma, to split the name and message
+        std::string received_str = msg.to_string();
+        auto split = received_str.find(",");
+
+        //Put the name before the comma
+        std::string sender_name = received_str.substr(0, split); 
+        
+        // Take the message out, after the comma
+        std::string actual_message = received_str.substr(split + 1);
+
+
         std::cout << "recieved message: '" << msg.to_string() << "'" << std::endl;
+
     }
 }
 
@@ -112,7 +129,7 @@ int main(int argc, char **argv)
     std::cout << "client successfully registered" << std::endl;
     std::cout << "to send a message type a message of the form: 'recipient,message' and then press enter" << std::endl;
 
-    std::thread send_thread(send_func,server_whereis_client_endpoint);
+    std::thread send_thread(send_func,server_whereis_client_endpoint, name);
     std::thread recv_thread(recv_func,recv_endpoint);
 
     send_thread.join();
